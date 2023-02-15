cef5af6c26cd83e8529ef3dee77374006d897006
blkmoose <36351254+heyligerjon@users.noreply.github.com>
2023-02-15 15:57:30 -0500

    updated models

--- network/models.py
+++ network/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Follower:
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    following = models.ManyToManyField(User)
+
+class Post:
+    postedBy = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField()
