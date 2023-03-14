e46894fce7b3e697faa2b227b8cbe6f771fad250
blkmoose <36351254+heyligerjon@users.noreply.github.com>
2023-03-14 17:03:34 -0400

    comments

--- network/views.py
+++ network/views.py
@@ -89,24 +89,24 @@ class ReactionViewSet(viewsets.ModelViewSet):
 #     else:
 #         return HttpResponseRedirect(reverse("login"))
 
-def login_view(request):
-    if request.method == "POST":
+# def login_view(request):
+#     if request.method == "POST":
 
-        # Attempt to sign user in
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
+#         # Attempt to sign user in
+#         username = request.POST["username"]
+#         password = request.POST["password"]
+#         user = authenticate(request, username=username, password=password)
 
-        # Check if authentication successful
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse("index"))
-        else:
-            return render(request, "network/login.html", {
-                "message": "Invalid username and/or password."
-            })
-    else:
-        return render(request, "network/login.html")
+#         # Check if authentication successful
+#         if user is not None:
+#             login(request, user)
+#             return HttpResponseRedirect(reverse("index"))
+#         else:
+#             return render(request, "network/login.html", {
+#                 "message": "Invalid username and/or password."
+#             })
+#     else:
+#         return render(request, "network/login.html")
 
 # def logout_view(request):
 #     logout(request)
